667534e805934f65fed4fc096d979156740546be
dev_kyn <wizardkyn@naver.com>
2016-03-03 02:18:41 +0900

    update .gitignore

---
+++ src/main/java/com/example/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value="/index.do", method=RequestMethod.GET)
+	public String hello(ModelMap model) {
+		return "index.jsp";
+	}
+}
